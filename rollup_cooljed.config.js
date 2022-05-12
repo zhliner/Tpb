85c2dbb8aacd4975281d2c427e3cdf9a7c4344f9
zhliner <zhliner@gmail.com>
2022-05-12 19:22:08 +0800

    configed for cooljed.

---
+++ rollup_cooljed.config.js
@@ -0,0 +1,19 @@
+import { terser } from 'rollup-plugin-terser'
+
+export default {
+    input: 'tpb.js',
+    output: {
+        file: '../cooljed/base/tpb/tpb.esm.js',
+        format: 'esm',
+        banner: '/*! Tpb/tQuery v0.4.1 | (c) zhliner@gmail.com 2021.12.05 | MIT License */',
+        sourcemapExcludeSources: true,
+    },
+    plugins: [
+        terser()
+    ],
+    // 排除工具集和配置文件捆绑。
+    external: [
+        './config.js',
+        /\/tools\/[\w.]+\.js/,
+    ],
+}
---
+++ tools/rollup_cooljed.config.js
@@ -0,0 +1,15 @@
+import { terser } from 'rollup-plugin-terser'
+
+export default {
+    input: [ 'date.js', 'ease.js', 'filter.js', 'hotkey.js', 'render.js', 'spliter.js', 'templater.js', 'tloader.js', 'util.js' ],
+    output: {
+        format: 'esm',
+        dir: '../../cooljed/base/tpb/tools',
+        preserveModules: true,
+        sourcemapExcludeSources: true,
+    },
+    plugins: [ terser() ],
+
+    // 排除上级配置文件（templater.js）
+    external: [ '../config.js' ]
+}
